034c9aeb00ce721c4a1cb3fa441aef63887d69d9
nuruddeen96 <60173558+deen96@users.noreply.github.com>
2020-12-24 23:30:59 +0300

    update 1

--- project-main/project/project00/Program.cs
+++ project-main/project/project00/Program.cs
@@ -30,7 +30,7 @@ namespace project00
             /////////////
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new loginform());
+            Application.Run(new ownerform());
            
            
         }
